8667d7572a6b82559a4bf65d0d13ca2d07a05943
Iyed13tns <m@g.com>
2025-01-17 23:49:06 +0100

    fix: improve print formatting and logic in multiple Python scripts

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-
 for i in range(0, 100):
     if i == 99:
-        i = "{:02d}".format(i)
-        end = None
+        print("{:02}".format(i))
     else:
-        i = "{:02d}".format(i)
-        end = ", "
-    print(i, end=end)
+        print("{:02d}".format(i), end=", ")
     
--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-for i in range(10):
+for i in range(0, 9):
     for j in range(i + 1, 10):
-        print("{}".format(i), end='')
-        if i == 8 and j == 9:
-            print("{}".format(j))
-        else:
-            print("{}".format(j), end=", ")
+        print("{}{}".format(i, j), end="\n" if i == 8 and j == 9 else ", "))
