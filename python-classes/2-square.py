15810b06e1cc55864eb5c32ea114ddfcf102a401
Iyed13tns <m@g.com>
2025-01-30 01:31:30 +0100

    fix: correct indentation in Square class for consistency

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,19 +1,16 @@
+
 #!/usr/bin/python3
 """Square class definition."""
 
 
 class Square:
-        """Represents a square."""
+    """Represents a square."""
 
-    
-def __init__(self, size):
+    def __init__(self, size):
         """Initialize with size validation."""
-        
-        
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
 
-        
         self.__size = size
