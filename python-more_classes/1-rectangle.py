36ba5e1e6b68f9d02ce6cdb712230b34e58df7ba
Iyed13tns <m@g.com>
2025-01-31 12:07:04 +0100

    refactor: simplify Rectangle class initialization and add width property

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,31 +5,14 @@ Definition of the Rectangle class
 
 
 class Rectangle:
-    """
-    Represents a Rectangle with private width and height attributes.
-    """
 
     def __init__(self, width, height):
-        """
-        Initializes a Rectangle instance with private width and height attributes.
-
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-
-        Raises:
-            TypeError: If width or height is not an integer.
-            ValueError: If width or height is negative.
-        """
-        if not isinstance(width, int):
-            raise TypeError("width must be an integer")
-        if width < 0:
-            raise ValueError("width must be >= 0")
         
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
-
+        
         self.__width = width
         self.__height = height
+
+
+    @property
+    def width(self, value):
+        return self.__width
