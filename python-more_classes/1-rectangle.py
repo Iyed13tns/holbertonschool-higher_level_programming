b44f83fcdf565e8c671893dc052939eaeb1d34f4
Iyed13tns <m@g.com>
2025-01-31 12:09:41 +0100

    refactor: enhance Rectangle class documentation and add attribute descriptions

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,18 +1,35 @@
 #!/usr/bin/python3
 """
-Definition of the Rectangle class
+Module contenant la définition de la classe Rectangle.
 """
 
 
 class Rectangle:
+    """
+    Classe représentant un rectangle.
+
+    Attributes:
+        __width (int): Largeur du rectangle (privée).
+        __height (int): Hauteur du rectangle (privée).
+    """
 
     def __init__(self, width, height):
-        
-        
+        """
+        Initialise un rectangle avec une largeur et une hauteur données.
+
+        Args:
+            width (int): La largeur du rectangle.
+            height (int): La hauteur du rectangle.
+        """
         self.__width = width
         self.__height = height
 
-
     @property
-    def width(self, value):
+    def width(self):
+        """
+        Getter pour la largeur du rectangle.
+
+        Returns:
+            int: La largeur du rectangle.
+        """
         return self.__width
