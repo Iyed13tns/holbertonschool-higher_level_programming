5ea92fdfd24bb64e09eb59f9eb8c9c1b90916172
Iyed13tns <m@g.com>
2025-02-13 11:12:13 +0100

    fix: add docstring to read_file function in 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,10 @@
-#!/usr/bin/python3
-def read_file(filename=""):
+#!/usr/bin/python3  # Spécifie l'interpréteur Python à utiliser
+
+def read_file(filename=""):  
+    """
+    Fonction qui lit un fichier et affiche son contenu.
+    
+    :param filename: Nom du fichier à lire (par défaut, chaîne vide)
+    """
     with open(filename, 'r', encoding='utf-8') as file:
          file.read(filename)
-    
