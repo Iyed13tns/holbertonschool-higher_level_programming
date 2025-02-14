66b093aedc3ff2792858346d8827e5a27ec30bb5
Iyed13tns <m@g.com>
2025-02-14 19:17:07 +0100

    fix: improve docstring and refactor read_file function for clarity and efficiency

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
-def read_file(filename=""):  
-    """
-    Function that reads a file and prints its content.
-    
-    :param filename: Name of the file to read (default is an empty string)
-    """
-    with open(filename, 'r', encoding='utf-8') as file:
-         content = file.read()
-         print(content)
+'''Task 00 - 0. Read file'''
+
+
+def read_file(filename=""):
+    '''function that reads a text file (UTF8)
+    and prints it to stdout:'''
+    with open(filename, mode='r', encoding='utf-8') as f:
+        print(f.read(), end="")
