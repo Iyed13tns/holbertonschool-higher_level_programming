0938be7303951e973d43589c7ecc55cf9ccab5f9
Iyed13tns <m@g.com>
2025-03-07 09:39:00 +0000

    docs: update SQL script to simplify SHOW GRANTS syntax for user_0d_1 and user_0d_2

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Content: SQL script with queries for privileges
-SHOW GRANTS FOR [user_0d_1]@[localhost];
-SHOW GRANTS FOR [user_0d_2]@[localhost];
+SHOW GRANTS [user_0d_1];
+SHOW GRANTS [user_0d_2];
