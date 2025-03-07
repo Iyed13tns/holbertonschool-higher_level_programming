b8024585b86fbc77bee8f85f03ead69bbc8dc06a
Iyed13tns <m@g.com>
2025-03-07 12:40:29 +0000

    docs: update SQL script to clarify purpose and adjust SHOW GRANTS for user_0d_2 to localhost

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- Content: SQL script with queries for privileges
+-- script to list privelages of users on server
+-- DML QUERY to do above
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'%';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
