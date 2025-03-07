21260eade8cd4425adb162868cfa627d187bcd62
Iyed13tns <m@g.com>
2025-03-07 09:16:55 +0000

    docs: update queries to select all records from user_0d_1 and user_0d_2 tables

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- Create a user_0d_1 table with the same structure as the user table.
-SELECT user, host FROM user_0d_1;
-SELECT user, host FROM user_0d_2;
+-- Create a user_0d_1 table with the same structure as the user table.SELECT user, host
+SELECT * FROM user_0d_1;
+SELECT * FROM user_0d_2;
