4de75ec22591b657159a91b9c7a2faf491e80def
Iyed13tns <m@g.com>
2025-03-25 18:23:37 +0100

    refactor: rename variable for clarity in argument count handling

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,17 +4,18 @@ import sys
 
 def main(*argv):
     i = 0
-    l = len(sys.argv) - 1
-    if l == 1:
-        print("{:d} argument:".format(l))
-    elif l == 0:
-        print("{:d} arguments.".format(l))
+    arg_count = len(sys.argv) - 1  # Renommé de 'l' à 'arg_count'
+    if arg_count == 1:
+        print("{:d} argument:".format(arg_count))
+    elif arg_count == 0:
+        print("{:d} arguments.".format(arg_count))
     else:
-        print("{:d} arguments:".format(l))
+        print("{:d} arguments:".format(arg_count))
     for args in sys.argv:
         if (i != 0):
             print("{}: {}".format(i, args))
         i += 1
+
 
 if __name__ == "__main__":
     main()
