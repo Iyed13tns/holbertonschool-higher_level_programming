42937d42cbecb1e60abddc6bef317703034c29be
Iyed13tns <m@g.com>
2025-03-26 16:40:28 +0100

    fix: reorder imports and format engine creation for better readability in 8-model_state_fetch_first.py

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,12 +1,11 @@
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-
 #!/usr/bin/python3
 """
 Script that prints the first State object from the database hbtn_0e_6_usa
 """
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 
 if __name__ == "__main__":
@@ -14,7 +13,9 @@ if __name__ == "__main__":
     password = sys.argv[2]
     db_name = sys.argv[3]
 
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}')
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}'
+    )
     Session = sessionmaker(bind=engine)
     session = Session()
 
