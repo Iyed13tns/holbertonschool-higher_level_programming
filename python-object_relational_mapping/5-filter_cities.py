83e64e4da720d20a175e3db4005517c9aa8400d2
Iyed13tns <m@g.com>
2025-03-26 16:23:58 +0100

    fix: update script documentation and format SQL query for readability

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Script that takes in the name of a state as an argument and lists all cities of that state
-from the database hbtn_0e_4_usa, using parameterized queries for security.
+Script that takes in the name of a state as an argument and lists all cities
+of that state from the database hbtn_0e_4_usa, using parameterized queries.
 """
 import sys
 import MySQLdb
@@ -10,18 +10,18 @@ if __name__ == "__main__":
     username, password, dbname, state_name = sys.argv[1:5]
 
     db = MySQLdb.connect(
-        host="localhost", 
-        port=3306, 
-        user=username, 
-        passwd=password, 
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
         db=dbname
     )
     cur = db.cursor()
     query = """
-    SELECT cities.name 
-    FROM cities 
-    JOIN states ON cities.state_id = states.id 
-    WHERE states.name = %s 
+    SELECT cities.name
+    FROM cities
+    JOIN states ON cities.state_id = states.id
+    WHERE states.name = %s
     ORDER BY cities.id ASC
     """
     cur.execute(query, (state_name,))
