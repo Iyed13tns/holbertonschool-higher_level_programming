967aab27b0f6cba17d3e4fdb810aae3b44c22a76
Iyed13tns <m@g.com>
2025-03-26 11:01:45 +0100

    style: format database connection code for improved readability

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,12 @@ import sys
 
 if __name__ == "__main__":
     # Connect to the database
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
 
     # Create a cursor object
     cursor = db.cursor()
