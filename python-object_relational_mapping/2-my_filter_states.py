6734569a62a7216ae03c701399d7ca1ca3bb86aa
Iyed13tns <m@g.com>
2025-03-26 15:44:27 +0100

    refactor: improve readability and structure of database query script

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -4,11 +4,31 @@ import sys
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4]))
-    rows = cur.fetchall()
+    # Get MySQL credentials and state name from command line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to the MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username, passwd=mysql_password, db=database_name)
+
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
+
+    # Create the SQL query using format to include the user input
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+
+    # Execute the query
+    cursor.execute(query)
+
+    # Fetch all the rows from the executed query
+    rows = cursor.fetchall()
+
+    # Print the rows
     for row in rows:
         print(row)
-    cur.close()
+
+    # Close the cursor and database connection
+    cursor.close()
     db.close()
