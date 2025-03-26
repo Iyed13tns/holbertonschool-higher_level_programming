2dda89f4d3fe4436881a391c38338b636457efe8
Iyed13tns <m@g.com>
2025-03-26 16:38:50 +0100

    feat: add script to fetch and display the first State object from MySQL database

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,27 @@
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+#!/usr/bin/python3
+"""
+Script that prints the first State object from the database hbtn_0e_6_usa
+"""
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    first_state = session.query(State).order_by(State.id).first()
+    if first_state:
+        print(f"{first_state.id}: {first_state.name}")
+    else:
+        print("Nothing")
+
+    session.close()
