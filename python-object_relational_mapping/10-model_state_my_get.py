d489a49f070b5d3c2cff3fdd34bd8a6d42294c45
Iyed13tns <m@g.com>
2025-03-26 17:02:42 +0100

    feat: improve script formatting and usage instructions for retrieving State object by name

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,18 +1,28 @@
+#!/usr/bin/python3
+"""
+Script that prints the State object with the name passed as argument
+from the database hbtn_0e_6_usa.
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-#!/usr/bin/python3
-
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        print("Usage: ./10-model_state_my_get.py <mysql username> <mysql password> <database name> <state name>")
+        print("Usage: ./10-model_state_my_get.py <mysql username> "
+              "<mysql password> <database name> <state name>")
         sys.exit(1)
 
-    username, password, dbname, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+    state_name = sys.argv[4]
 
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{dbname}', pool_pre_ping=True)
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{dbname}',
+        pool_pre_ping=True
+    )
     Session = sessionmaker(bind=engine)
     session = Session()
 
