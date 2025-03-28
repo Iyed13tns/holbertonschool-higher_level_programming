e0be0136fca08441e2c6ba83cc02216e6fb12b4b
Iyed13tns <m@g.com>
2025-03-28 10:54:41 +0100

    feat: add comments to id_not_null table creation script for clarity

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,5 @@
+-- Script that creates a table 'id_not_null' with id and name columns
+-- The id column cannot be null and has a default value of 1
 CREATE TABLE IF NOT EXISTS id_not_null (
     id INT NOT NULL DEFAULT 1,
     name VARCHAR(256)
