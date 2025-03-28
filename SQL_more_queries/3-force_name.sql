5a6af920ac5b23651c225cf846ffcfd8891eb80b
Iyed13tns <m@g.com>
2025-03-28 10:51:50 +0100

    feat: add comments to force_name table creation script for clarity

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,3 +1,5 @@
+-- Script that creates a table 'force_name' with id and name columns
+-- The name column cannot be null
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL
