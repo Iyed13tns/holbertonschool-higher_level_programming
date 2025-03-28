6988aa2e3ac8b5fa3f8fb48795276a828eef45df
Iyed13tns <m@g.com>
2025-03-28 10:48:22 +0100

    feat: add comments to user creation script for clarity

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
+-- Script that creates the MySQL server user user_0d_1 with all privileges
+-- and sets password to user_0d_1_pwd
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
