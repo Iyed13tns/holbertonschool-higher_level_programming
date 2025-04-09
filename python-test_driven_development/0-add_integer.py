c42fb8624fafe32563938ec99b6d6a0efd1b7237
Iyed13tns <m@g.com>
2025-04-08 23:39:25 -0400

    fix: update documentation for add_integer function to clarify argument types and return value

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python3
 """
 Module 0-add_integer
-Ce module fournit une fonction pour ajouter deux entiers.
+Ce module fournit une fonction pour additionner deux entiers.
 """
 
 def add_integer(a, b=98):
     """
-    Ajoute deux nombres entiers.
-    
+    Ajoute deux nombres entiers ou flottants.
     Arguments :
-    a : Un entier ou un flottant, obligatoire.
-    b : Un entier ou un flottant, optionnel, valeur par défaut 98.
+    - a : Un entier ou flottant (obligatoire)
+    - b : Un entier ou flottant (par défaut 98)
     
     Retourne :
-    La somme des deux entiers après conversion en entier.
+    - La somme de `a` et `b` en tant qu'entiers.
     
     Exceptions :
-    Lève une exception TypeError si les arguments ne sont pas de type valide.
+    - Lève une exception TypeError si `a` ou `b` ne sont pas valides.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
     return int(a) + int(b)
