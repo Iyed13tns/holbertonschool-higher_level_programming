a6f421df4106859f73c0fb0eeed3b9ee721ece2f
Iyed13tns <m@g.com>
2025-05-06 21:49:43 +0200

    Change file permissions and clean up import statements in 1-calculation.py

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,6 +1,5 @@
-from calculator_1 import add, sub, mul, div
-
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
 
@@ -10,4 +9,4 @@ if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    
