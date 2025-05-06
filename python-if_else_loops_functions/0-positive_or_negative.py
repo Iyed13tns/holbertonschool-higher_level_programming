61227ad5b41752e123bda739f38207aa522062d2
Iyed13tns <m@g.com>
2025-05-06 09:11:36 +0200

    Enhance 0-positive_or_negative.py with comments for clarity on random number assignment and checks

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 import random
 
-
+# Assign a random signed number to the variable number
 number = random.randint(-10, 10)
 
+# Check if the number is positive, zero, or negative
 if number > 0:
     print(f"{number} is positive")
 elif number == 0:
