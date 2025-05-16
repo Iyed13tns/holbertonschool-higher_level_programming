de6e9c3fccbf5709166eedfe8e15901a07626392
Iyed13tns <m@g.com>
2025-05-16 15:54:25 +0200

    Refactor docstring in add_integer function for clarity and accuracy

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 """
-This module provides a function to add two integers.
-
-It ensures that inputs are integers or floats,
-and raises a TypeError if they are not.
+Function that adds 2 integers.
+a and b must be integers or floats
+a and b must be first casted to integers if they are float
 """
 
 
 def add_integer(a, b=98):
-    """
-    Adds two integers or floats as integers.
-
-    Returns the sum as an integer.
+    """ Add to integers or floats
+        a: first number
+        b: second number
+        Return: the result of the addition of two a and b
+        Raise: TypeError if a or b is not an integer or float
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
-    return int(a) + int(b)
+    return int(a + b)
