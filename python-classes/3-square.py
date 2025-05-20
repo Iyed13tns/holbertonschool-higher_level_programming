287439dd39f46e0951f5efcb5df96bf053494323
Iyed13tns <m@g.com>
2025-05-21 00:29:09 +0200

    Enhance Square class with area method and improve size validation in constructor

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,15 +1,29 @@
+#!/usr/bin/python3
+"""Defines a class Square."""
+
+
 class Square:
-    """Class that defines a square."""
+    """Represents a square."""
 
     def __init__(self, size=0):
         """
         Initialize a new Square.
 
         Args:
-            size (int): The size of the square. Defaults to 0.
+            size (int): The size of the square. Must be >= 0.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        """
+        Calculate the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
+    
