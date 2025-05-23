55ab6c2acbf9dbab0bdece48299d265031b043a8
Uyed Debara <iyeddebara@icloud.com>
2025-05-23 10:59:20 +0200

    Fix string representation in Rectangle class

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -56,5 +56,4 @@ class Rectangle:
 
     def __repr__(self):
         """Return a string representation for recreating the rectangle."""
-        return f"Rectangle({self.width}, {self.height})"
-    
+        return f"Rectangle({
