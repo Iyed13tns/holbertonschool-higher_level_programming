46bbbd0410bc4f4233bcf9af93ef7163a6e0f19b
Uyed Debara <iyeddebara@icloud.com>
2025-05-26 17:17:50 +0200

    Update docstring in is_same_class function for formatting consistency

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-This module defines a function 
-to check if an object is exactly 
+This module defines a function
+to check if an object is exactly
 an instance of a specified class.
 """
 
