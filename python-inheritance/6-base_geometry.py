cb0aa52dcaf47da5bb3e234638a7e736ce978df8
Uyed Debara <iyeddebara@icloud.com>
2025-05-27 14:14:39 +0200

    Add empty BaseGeometry class with area method placeholder

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+This module defines an empty class BaseGeometry
+'''
+
+
+class BaseGeometry:
+    """
+    jj
+    """
+    pass
+
+def area(self):
+    raise Exception("area() is not implemented")
+
