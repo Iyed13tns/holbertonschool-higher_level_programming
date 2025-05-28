a784257c369216b53a6a15eb9bc3198ff6efbbd9
Uyed Debara <iyeddebara@icloud.com>
2025-05-28 11:27:26 +0200

    Add area method to BaseGeometry class and create new BaseGeometry module

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+This module defines an empty class BaseGeometry
+'''
+
+
+class BaseGeometry:
+    '''
+    f
+    '''
+    def area(self):
+        '''
+        h
+        '''
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """
+        f
+        """
+        if not isinstance(value, int):
+           raise TypeError(f"{name} must be an integer")
+        
+        if value <= 0:
+           raise ValueError(f"{name} must be greater than 0")
