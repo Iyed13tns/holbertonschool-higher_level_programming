4f590ed6bef53d7aeb0baa3f75cdd8b1bb90d56f
Uyed Debara <iyeddebara@icloud.com>
2025-06-06 15:37:50 +0200

    Fix import statements in 7-add_item.py to remove unnecessary relative paths

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,11 +3,10 @@
 This script adds all arguments to a Python list and saves them to a file.
 """
 
-
 import sys
 from os import path
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
 filename = "add_item.json"
 
