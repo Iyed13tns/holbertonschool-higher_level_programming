9835fe88a7f65114d66c850aafb50d8e01446443
Uyed Debara <iyeddebara@icloud.com>
2025-06-06 15:04:15 +0200

    Update docstring in read_file function for clarity

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,12 +4,14 @@ This module contains a function that reads a text file (UTF8)
 and prints it to stdout.
 """
 
+
 def read_file(filename=""):
     """
     Reads a text file (UTF8) and prints it to stdout.
 
     Args:
-        filename (str): The name of the file to read. Defaults to an empty string.
+        filename (str): The name of the file to read.
+        Defaults to an empty string.
     """
     with open(filename, "r", encoding="utf-8") as file:
         print(file.read(), end="")
