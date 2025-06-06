a90e101e43d8cbe332dea65ffedcfb749aa59659
Uyed Debara <iyeddebara@icloud.com>
2025-06-06 15:21:33 +0200

    Fix docstring formatting in append_write.py for improved readability

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """
-This module contains a function that appends a string at the end of a text file (UTF8)
-and returns the number of characters added.
+This module contains a function that appends a string at the end of a text file
+(UTF8) and returns the number of characters added.
 """
 
 
 def append_write(filename="", text=""):
     """
-    Appends a string at the end of a text file (UTF8) and returns the number of characters.
+    Appends a string at the end of a text file (UTF8) and returns the number
+    of characters.
 
     Args:
         filename (str): The name of the file to append to.
