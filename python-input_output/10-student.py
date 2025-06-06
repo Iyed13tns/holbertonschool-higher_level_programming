ca040d3c4162d3d6d8c4b61160a6cb8baae8c4fe
Uyed Debara <iyeddebara@icloud.com>
2025-06-06 15:57:34 +0200

    Refactor JSON serialization method in Student class for improved readability

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -35,5 +35,6 @@ class Student:
         """
         if attrs is None:
             return self.__dict__
-        return {key: value for key, value in self.__dict__.items() if key in attrs}
-    
+        return {
+            key: value for key, value in self.__dict__.items() if key in attrs
+        }
