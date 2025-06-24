d49bf69c246a29846c8e559aa0258279720f9cc9
Uyed Debara <iyeddebara@icloud.com>
2025-06-24 16:08:39 +0200

    Add SQL script to create user_0d_1 with all privileges on localhost

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
--- Script to list all privileges of MySQL users user_0d_1 and user_0d_2 on localhost
-
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- creates the MySQL server user user_0d_1
+CREATE USER if NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
