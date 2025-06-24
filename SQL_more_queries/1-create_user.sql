800f7c21c897dc05767c35559c5651c9fb2eda4a
Uyed Debara <iyeddebara@icloud.com>
2025-06-24 16:03:09 +0200

    Add SQL script to create a user with all privileges on all databases

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- This script creates a user with all privileges on all databases and tables.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'%' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'%';
+FLUSH PRIVILEGES;
