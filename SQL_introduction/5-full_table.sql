d2cda737bb83dcd71370b03d0351503390e7bea9
Uyed Debara <iyeddebara@icloud.com>
2025-06-24 14:37:48 +0200

    Replace query with SHOW CREATE TABLE statement for first_table in full_table.sql

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,9 +1,2 @@
--- file: 5-full_table.sql
-SELECT 
-    TABLE_NAME AS 'Table',
-    CREATE_TABLE AS 'Create Table'
-FROM 
-    INFORMATION_SCHEMA.TABLES
-WHERE 
-    TABLE_SCHEMA = DATABASE()
-    AND TABLE_NAME = 'first_table';
+-- prints description of the table first_table
+SHOW CREATE TABLE first_table;
